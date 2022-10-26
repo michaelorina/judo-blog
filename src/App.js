2e182258d010c30106e10913093d861d9c91987c
michaelorina <orinamichael4@gmail.com>
2022-10-26 11:20:28 +0300

    Wed Oct 26 Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <div className ="component">
-        <h1> App Component</h1>
+      <Navbar/>
+      <div className ="content">
+        <Home/>
       </div>      
     </div>
   );
