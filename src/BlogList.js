328ef7f832cd4088305ddb1fa8520b0456de0d5c
michaelorina <orinamichael4@gmail.com>
2022-11-01 11:43:38 +0300

    nov 1 commit

---
+++ src/BlogList.js
@@ -0,0 +1,19 @@
+const BlogList = ({blogs, title}) => {
+
+    // const blogs = props.blogs;
+    // const title = props.title;
+
+    return(
+        <div className="blog-list">
+            <h1>{ title }</h1>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2> {blog.title} </h2>
+                    <p> Written by {blog.author} </p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
+import {useState} from 'react'
+import BlogList from './BlogList';
+
 const Home = () => {
+    
+    const[blogs, setBlogs] = useState([
+        {title:'Best Hacking Websites', body:'60 Sticks per box', author:'Michael', id: 1},
+        {title:'Is it too Late to Start', body:'Tap to Save', author:'Louisa', id: 2},
+        {title:'Top 10 AI firms', body:'Like am gonna loose you', author:'Louisa', id: 3}
+    ]);
     return (
         <div className ="home">
-            <h2>Home Page</h2>
+            <BlogList blogs = {blogs} title = "All Blogs!"/>
+            <BlogList blogs = {blogs.filter((blogs) => blogs.author === 'Louisa')} title = "Louisa Blogs!"/>
         </div>
     );
 }
